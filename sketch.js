7d9338c0e66d9f7766659b2a5a6a5a93cac45d68
VishalM2406 <77235892+VishalM2406@users.noreply.github.com>
2021-06-19 12:53:57 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
+
+async function getBackgroundImg(){
+  var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+  var responseJSON = await response.json();
+
+  var datetime = responseJSON.datetime;
+  var hour = datetime.slice(11,13);
+  
+  if(hour>=0600 && hour<=1900){
+      bg = "snow1.jpg";
+  }
+  else{
+      bg = "snow2.jpg";
+  }
+
+  backgroundImg = loadImage(bg);
+  console.log(backgroundImg);
+  
+}
